257760d82a7f5012a449e85af5daef82c2ecc2c5
hj <a301dks@naver.com>
2020-08-06 15:18:40 +0900

    test: 경로 조회 결과가 요금 정보를 반환는 인수 테스트 작성

--- src/main/java/wooteco/subway/maps/map/dto/PathResponseAssembler.java
+++ src/main/java/wooteco/subway/maps/map/dto/PathResponseAssembler.java
@@ -15,7 +15,8 @@ public class PathResponseAssembler {
                 .collect(Collectors.toList());
 
         int distance = subwayPath.calculateDistance();
+        final int fare = 1250;
 
-        return new PathResponse(stationResponses, subwayPath.calculateDuration(), distance);
+        return new PathResponse(stationResponses, subwayPath.calculateDuration(), distance, fare);
     }
 }
